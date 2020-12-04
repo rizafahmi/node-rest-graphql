e4c5cc607297e2e0b5f15ac80bfcce15e949e0e7
Riza Fahmi <rizafahmi@gmail.com>
2020-12-04 22:08:48 +0700

    GraphQL Hello world

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLSchema,GraphQLObjectType, GraphQLString } from 'graphql';
 
 import router from './router.js';
 
@@ -7,9 +9,27 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "RootQueryType",
+        fields: {
+            hello: {
+                type: GraphQLString,
+                resolve() {
+                    return 'Hello, GraphQL!';
+                }
+            }
+        }
+    })
+})
+
 app.get('/', function(req, res) {
     res.json({status: "OK"})
-})
+});
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 app.use(router);
 
 app.listen(3000, function() {
